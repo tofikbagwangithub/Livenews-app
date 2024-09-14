f11a870f265da86b6dd7e9bf93e4bce95cb38cd5
tofikbagwangithub <tofik.bagwan10@gmail.com>
2024-09-14 12:25:23 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,22 @@ function App() {
 
   useEffect(()=> {
     fetch(`https://newsapi.org/v2/everything?q=${category}&from=2024-09-12&apiKey=7af293154a7a46aeb929b9e31940adbf`)
-    .then((response)=>response.json())
+    .then((response)=>{
+      if(!response.ok){
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
     .then((news)=>{
-      setArticles(news.articles);
-      console.log(news.articles);
+      if(news){
+        setArticles(news.articles);
+        console.log(news.articles); 
+      }else{
+        console.error('Received undefined or empty data');
+      }
     })
     .catch((err)=>{
-      console.log(err);
+      console.log('Error fetching data:', err);
     })
   }, [category])
 
